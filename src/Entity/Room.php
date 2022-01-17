ce631fb4d63f34ee2d44779961d6babb1b733b24
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-01-17 12:22:48 +0100

    made a entity for room and controller

---
+++ src/Entity/Room.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RoomRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RoomRepository::class)]
+class Room
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $onlyForPremiumMembers;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getOnlyForPremiumMembers(): ?string
+    {
+        return $this->onlyForPremiumMembers;
+    }
+
+    public function setOnlyForPremiumMembers(string $onlyForPremiumMembers): self
+    {
+        $this->onlyForPremiumMembers = $onlyForPremiumMembers;
+
+        return $this;
+    }
+}
