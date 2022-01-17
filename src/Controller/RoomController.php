2c8c5b0ea4b7b6662120488e20aa5f10782ebbdc
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-01-17 13:07:35 +0100

    made it possible to add somethings in database room and data is out in array by findall metode from room repository

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -2,17 +2,44 @@
 
 namespace App\Controller;
 
+use App\Entity\Room;
+use App\Repository\RoomRepository;
+use JetBrains\PhpStorm\Pure;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
+#[Route('/room', name: 'room.')]
 class RoomController extends AbstractController
 {
-    #[Route('/room', name: 'room')]
-    public function index(): Response
+    #[Route('/', name: 'edit')]
+    public function index(RoomRepository $roomRepo): Response
     {
+        $rooms = $roomRepo->findAll();
+
         return $this->render('room/index.html.twig', [
-            'controller_name' => 'RoomController',
+            'rooms' => $rooms
         ]);
+    }
+
+    #[Route('/add', name: 'add')]
+    public function add(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $room = new Room();
+        $room->setName('one');
+        $room->setOnlyForPremiumMembers('false');
+
+        //entitymanager
+        $em = $doctrine->getManager();
+        //store
+        $em->persist($room);
+        //send
+        $em->flush();
+
+
+        //reponse
+        return new Response("Room has been created");
     }
 }
