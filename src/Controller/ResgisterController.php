bbb3b58afc750bdf983b424a1f5ae706b4d13280
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-01-18 11:18:32 +0100

    made a new controller resgister and form

---
+++ src/Controller/ResgisterController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ResgisterController extends AbstractController
+{
+    #[Route('/reg', name: 'reg')]
+    public function index(): Response
+    {
+        return $this->render('register/index.html.twig', [
+
+        ]);
+    }
+}
